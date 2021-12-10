afd506b233d098dfba0ec666d72c25055cf4df0b
AustinRansford <89941018+AustinRansford@users.noreply.github.com>
2021-12-09 22:41:21 -0800

    MileStone 2 shops buttons

--- CharacterCreator/Assets/Scripts/PlayerController.cs
+++ CharacterCreator/Assets/Scripts/PlayerController.cs
@@ -4,20 +4,40 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
-    public SpriteRenderer ArmorRenderer;
+    public GameObject StartScreen;
+    public GameObject ArmorShop;
+    public GameObject WeaponShop;
+    public SpriteRenderer ArmorRenderer; 
+    public SpriteRenderer WeaponRenderer;
     
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
-        
+        this.StartScreen.SetActive(true);
+ this.WeaponShop.SetActive(false);
+ this.ArmorShop.SetActive(false);
     }
     public void SetArmor(Sprite newSprite)
     {
         ArmorRenderer.sprite = newSprite;
     }
+    public void SetWeapon(Sprite newWeapon){
+        this.WeaponRenderer.sprite = newWeapon;
+    }
 
     // Update is called once per frame
+    public void ArmorShop1(){
+ this.ArmorShop.SetActive(true);
+ this.WeaponShop.SetActive(false);
+   this.StartScreen.SetActive(false);
+
+
+    }
+    public void WeaponShop1(){
+ this.ArmorShop.SetActive(false);
+  this.WeaponShop.SetActive(true);
+  this.StartScreen.SetActive(false);
+    }
     void Update()
     {
         
